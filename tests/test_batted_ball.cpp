0360d4bb73c0d3a5f584394ebeaa369e5e180e18
Apollo <acreyes311@yahoo.com>
2026-09-16 19:41:37 -0700

    Split build into xba_core library and set up Catch2 via FetchContent

---
+++ tests/test_batted_ball.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "batted_ball.hpp"
+
+TEST_CASE("placeholder", "[batted_ball]") {
+    REQUIRE(1 == 1);
+}
---
+++ tests/test_bucket_model.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "bucket_model.hpp"
+
+TEST_CASE("placeholder", "[bucket_model]") {
+    REQUIRE(1 == 1);
+}
